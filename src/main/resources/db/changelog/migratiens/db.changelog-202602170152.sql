e49e2e7fba2d2d1ebd8e429f6fc7a4d546d2797d
Daniel Victor <danielhtinho1227@gmail.com>
2026-02-17 02:09:27 -0300

    Initial commit

---
+++ src/main/resources/db/changelog/migratiens/db.changelog-202602170152.sql
@@ -0,0 +1,11 @@
+--Liquibase formatted sql
+--changest daniel:202602170152
+--comment: boards table create
+
+CREATE TABLE BOARDS(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
+
+
+--rollback DROP TABLE BOARDS
