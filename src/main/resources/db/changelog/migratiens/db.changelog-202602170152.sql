d41dbd83b7f746e8ea2a6e09ba073d908c76aa69
Daniel Victor <danielhtinho1227@gmail.com>
2026-02-17 02:25:39 -0300

    conexão com postgresSQL

--- src/main/resources/db/changelog/migratiens/db.changelog-202602170152.sql
+++ src/main/resources/db/changelog/migratiens/db.changelog-202602170152.sql
@@ -1,11 +1,10 @@
---Liquibase formatted sql
---changest daniel:202602170152
+--liquibase formatted sql
+--changeset daniel:202602170152
 --comment: boards table create
 
 CREATE TABLE BOARDS(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
-) ENGINE=InnoDB;
-
+);
 
 --rollback DROP TABLE BOARDS
